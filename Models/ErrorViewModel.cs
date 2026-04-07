cd6dc08034e1c0d0f6026f18d7aac529b7581b7c
ST10341577-EMERIS <st10341577@myemeris.edu.za>
2026-04-07 20:12:32 +0200

    Project Creation
    I've created my ASP.NET Core MVC project for the ice task

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommerceMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
